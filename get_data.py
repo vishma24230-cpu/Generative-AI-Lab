4211a8c167b434ba81c9b8ae2a4b776cf8aa217b
vishma24230-cpu <vishma24230@iiitd.ac.in>
2025-12-20 13:03:07 +0000

    Basic st and langchain usage added

--- get_data.py
+++ get_data.py
@@ -1,5 +1,9 @@
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from sklearn.metrics.pairwise import cosine_similarity
+import streamlit as st
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, GoogleGenerativeAI
+from streamlit.runtime.state import session_state
+from get_data import get_relevant
+
 
 def get_relevant(query, documents):
   '''
@@ -10,21 +14,50 @@ def get_relevant(query, documents):
   OUTPUT:
   return most similar item in document,  similarity score of most similar item in document
   '''
-  embeddings=GoogleGenerativeAIEmbeddings(model="gemini-embedding-001")
-  query_embedding=embeddings.embed_query(query)
-  document_embeddings=embeddings.embed_documents(documents) 
-  similarities=cosine_similarity([query_embedding],document_embeddings)[0]
-  similarities=sorted(list(enumerate(similarities)), key= lambda x:x[1])
+  embeddings = GoogleGenerativeAIEmbeddings(model="gemini-embedding-001")
+  query_embedding = embeddings.embed_query(query)
+  document_embeddings = embeddings.embed_documents(documents)
+  similarities = cosine_similarity([query_embedding], document_embeddings)[0]
+  similarities = sorted(list(enumerate(similarities)), key=lambda x: x[1])
   return documents[similarities[-1][0]], similarities[-1][1]
 
-if __name__=="__main__":
-  documents=[
-                   "Virat Kohli is a great cricketer, he's one of the youngest to score a century in test cricket",
-                   "Rohit Sharma is the current captain of the Indian cricket team",
-                   "MS Dhoni is a former captain of the Indian cricket team, also known as the captain cool or thala",
-                   "Sachin Tendulkar is the greatest batsman of all time, he's known as the god of cricket",
-                   "Ravindra Jadeja is an all-rounder, he's known for his bowling and batting skills",
-                   "Jasprit Bumrah is a fast bowler, he's known for his yorkers",
-                   "Mark Zuckerberg is the CEO of Facebook"
+
+def test_fun():
+  documents = [
+      "Virat Kohli is a great cricketer, he's one of the youngest to score a century in test cricket",
+      "Rohit Sharma is the current captain of the Indian cricket team",
+      "MS Dhoni is a former captain of the Indian cricket team, also known as the captain cool or thala",
+      "Sachin Tendulkar is the greatest batsman of all time, he's known as the god of cricket",
+      "Ravindra Jadeja is an all-rounder, he's known for his bowling and batting skills",
+      "Jasprit Bumrah is a fast bowler, he's known for his yorkers",
+      "Mark Zuckerberg is the CEO of Facebook"
   ]
   print(get_relevant("Who is virat kohli", documents))
+
+
+if __name__ == "__main__":
+  # test_fun() -> uncomment and check input to test the fgun
+  st.write("Data we have:")
+  documents = [
+      "Virat Kohli is a great cricketer, he's one of the youngest to score a century in test cricket",
+      "Rohit Sharma is the current captain of the Indian cricket team",
+      "MS Dhoni is a former captain of the Indian cricket team, also known as the captain cool or thala",
+      "Sachin Tendulkar is the greatest batsman of all time, he's known as the god of cricket",
+      "Ravindra Jadeja is an all-rounder, he's known for his bowling and batting skills",
+      "Jasprit Bumrah is a fast bowler, he's known for his yorkers",
+      "Mark Zuckerberg is the CEO of Facebook"
+  ]
+  st.write(documents)
+
+  query = st.text_area(label="Enter your question here",
+                       value="Your question e.g. What is this document about?")
+
+  if "result" not in st.session_state:
+    st.session_state.result = ""
+
+  st.button(label="Submit",
+            on_click=lambda: st.session_state.update(
+                {"result": get_relevant(query, documents)}))
+  st.button(label="reset",
+            on_click=lambda: st.session_state.update({"result": ""}))
+  st.write(f"Most relevant vector: {st.session_state.result}")
