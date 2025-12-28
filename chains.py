d2c6ae3f60aeefcb305af79ccd6d96d5c22f7241
vishma24230-cpu <vishma24230@iiitd.ac.in>
2025-12-28 14:50:48 +0000

    Added correct sextions for strwwmlit text, and stuck to the policy 'import what you use' by discarding unwanted imports.

--- chains.py
+++ chains.py
@@ -1,8 +1,6 @@
-from langchain_core import output_parsers
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from sqlalchemy import MergedResult
 import streamlit as st
 from langchain_core.runnables import RunnableParallel
 
@@ -48,58 +46,66 @@ with st.container():
 
 st.subheader("2) Parallel chains")
 st.text("Chain 1 and chain 2 runs in parallel.")
-with st.container():
+st.text("The below code was made using streamlit and parallel chains.")
+with st.expander("Details"):
+  with st.container():
+    st.code(body='''
+      # Code at {LINK}
+      # TODO: Update link
+      ''',
+            language='Python')
+  with st.container():
 
-  # st.code(body='''
-  # ''', language='Python', line_numbers=True)
+    # st.code(body='''
+    # ''', language='Python', line_numbers=True)
 
-  essay_prompt = PromptTemplate(template='''
-    You are an AI assistant. Generate an essay within int(2000*3/2) words on the topic {topic}.
+    essay_prompt = PromptTemplate(template='''
+      You are an AI assistant. Generate an essay within int(2000*3/2) words on the topic {topic}.
+      ''',
+                                  input_variables=["topic"])
+
+    quiz_prompt = PromptTemplate(template='''
+    You are an AI assistant:
+    Generate a quiz based on the topic.
+  
+    topic: {topic}
     ''',
-                                input_variables=["topic"])
+                                 input_variables=["topic"])
 
-  quiz_prompt = PromptTemplate(template='''
-  You are an AI assistant:
-  Generate a quiz based on the topic.
+    final_prompt = PromptTemplate(template='''
+    You are an AI assistant. Look the the given essay and quiz, and add information to the essay if any quiz questions are unrelated.
+    Finally format the final essay and quiz into parts and give the final output.
+  
+    essay: 
+    {essay}
+  
+    quiz: 
+    {quiz}
+    ''',
+                                  input_variables=["essay", "quiz"])
 
-  topic: {topic}
-  ''',
-                               input_variables=["topic"])
+    model = ChatGoogleGenerativeAI(model='gemini-2.5-flash',
+                                   model_kwargs={"max_output_tokens": 2000})
 
-  final_prompt = PromptTemplate(template='''
-  You are an AI assistant. Look the the given essay and quiz, and add information to the essay if any quiz questions are unrelated.
-  Finally format the final essay and quiz into parts and give the final output.
+    parser = StrOutputParser()
 
-  essay: 
-  {essay}
+    st.text(
+        "Enter a topic you want to study. The AI assistant genrates an essay which you can rerad, and then generates a quiz you can answer."
+    )
 
-  quiz: 
-  {quiz}
-  ''',
-                                input_variables=["essay", "quiz"])
+    topic = st.text_area("Enter a topic you want to study.")
 
-  model = ChatGoogleGenerativeAI(model='gemini-2.5-flash',
-                                 model_kwargs={"max_output_tokens": 2000})
+    if st.button("Send"):
+      if topic.strip() == "":
+        st.error("Please enter a valid topic.")
+      elif len(topic.strip()) <= 3:
+        st.error("Please describe the topic a bit more.")
+      else:
+        parallel_chain = RunnableParallel({
+            "essay": essay_prompt | model | parser,
+            "quiz": quiz_prompt | model | parser
+        })
+        merge_chain = final_prompt | model | parser
+        final_chain = parallel_chain | merge_chain | parser
 
-  parser = StrOutputParser()
-
-  st.text(
-      "Enter a topic you want to study. The AI assistant genrates an essay which you can rerad, and then generates a quiz you can answer."
-  )
-
-  topic = st.text_area("Enter a topic you want to study.")
-
-  if st.button("Send"):
-    if topic.strip() == "":
-      st.error("Please enter a valid topic.")
-    elif len(topic.strip()) <= 3:
-      st.error("Please describe the topic a bit more.")
-    else:
-      parallel_chain = RunnableParallel({
-          "essay": essay_prompt | model | parser,
-          "quiz": quiz_prompt | model | parser
-      })
-      merge_chain = final_prompt | model | parser
-      final_chain = parallel_chain | merge_chain | parser
-
-      st.text(final_chain.invoke({"topic": topic}))
+        st.text(final_chain.invoke({"topic": topic}))
